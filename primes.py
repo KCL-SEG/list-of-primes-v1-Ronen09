9af0944b7d8b40af071b3069db9648c58319949b
Ronen09 <ronen.roy@kcl.ac.uk>
2022-09-29 10:50:11 +0100

    modified primes.py

--- primes.py
+++ primes.py
@@ -3,4 +3,17 @@
 
 def primes(number_of_primes):
     list = []
+    i = 2;
+    while True:
+        if check_prime(i):
+            list.append(i)
+        if len(list) == number_of_primes:
+            return list
+        i += 1
     return list
+
+def check_prime(number):
+    for i in range(2,number):
+        if number % i == 0:
+            return False
+    return True
